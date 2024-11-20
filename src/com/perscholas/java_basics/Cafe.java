93ee0d0300c4e91c6886e90d88fca1a65246254b
sravana karne <sandhyajava42@gmail.com>
2024-11-19 21:30:26 -0600

    Operators and Variables pracice exrercise made changes committing

--- src/com/perscholas/java_basics/Cafe.java
+++ src/com/perscholas/java_basics/Cafe.java
@@ -4,52 +4,28 @@ import java.util.Scanner;
 
 public class Cafe {
 
-        Scanner sc = new Scanner(System.in);
+        //Scanner sc = new Scanner(System.in);
          double subtotal;
          double totalsale;
-         final double salestax=2;
+         final double salestax=2.05;
         double coffeeprice= 3.5;
         double teaprice=3.2;
         double greenteaprice=2.4;
-        int orders;
+       // int orders;
         double cofeeorders;
         double teaorders;
         double greenteaorders;
-
-        public void coffee(){
-            System.out.println("Enter the number of coffee orders");
-            orders = sc.nextInt();
-            cofeeorders=orders*coffeeprice;
-            System.out.println("total cofeeorders "+cofeeorders);
-            //sc.close();
-        }
-        public void tea(){
-            System.out.println("Enter the number of tea orders");
-            orders = sc.nextInt();
-            teaorders=orders*teaprice;
-            System.out.println("total teaorders "+teaorders);
-            //sc.close();
-        }
-        public void greentea() {
-            System.out.println("Enter the number of green tea orders");
-            orders = sc.nextInt();
-            greenteaorders = orders * greenteaprice;
-            System.out.println("total greenteaorders "+greenteaorders);
-            //sc.close();
-        }
-        public void subtotal() {
-            subtotal=0;
-            //System.out.println("Enter the total subtotal" + subtotal);
-            subtotal=cofeeorders+teaorders+greenteaorders;
-            System.out.println("subtotal is "+subtotal);
-        }
-        public void totalsale() {
-            totalsale=0;
-            //System.out.println("Enter the total sales" + totalsale);
-            totalsale=subtotal+salestax;
-            System.out.println("totalsale is "+totalsale);
-            sc.close();
-        }
-
+    public void printOrders() {
+        cofeeorders=3*coffeeprice;
+        teaorders=2*teaprice;
+        greenteaorders=4*greenteaprice;
+        subtotal=cofeeorders+teaorders+greenteaorders;
+        System.out.println("coffee orders total is " +cofeeorders);
+        System.out.println("tea orders total is " +teaorders);
+        System.out.println("green tea orders total is " +greenteaorders);
+        System.out.println("orders subtotal is  " +subtotal);
+        totalsale=subtotal+salestax;
+        System.out.println("total sale value is " +totalsale);
     }
+}
 
--- src/com/perscholas/java_basics/Main.java
+++ src/com/perscholas/java_basics/Main.java
@@ -4,10 +4,6 @@ public class Main {
     public static void main(String[] args) {
 
         Cafe cafeOrders = new Cafe();
-        cafeOrders.coffee();
-        cafeOrders.greentea();
-        cafeOrders.tea();
-        cafeOrders.subtotal();
-        cafeOrders.totalsale();
+       cafeOrders.printOrders();
     }
 }
